e2b5f5f6a417bbaeb780096e4bb1b8d460c68a96
Andres <dresaan@gmail.com>
2024-06-26 14:55:44 +0200

    initial set up for exercises

---
+++ Part1/00_course/part1/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
