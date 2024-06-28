3df2ca2bb609254674d763d891db1f5a26cc44b6
Andres <dresaan@gmail.com>
2024-06-28 20:06:17 +0200

    Finished the lesson

--- Part1/course_info/src/App.jsx
+++ Part1/course_info/src/App.jsx
@@ -1,11 +1,19 @@
-const App = () => {
+const App = () => {  
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   /* create the Header component that receives as prompt the course */
   const Header = (props) => {    
@@ -23,30 +31,32 @@ const App = () => {
     /*console.log('part is called')*/
     return(
       <div>
-        {props.part} {props.exercises}
+        {props.part.name} {props.part.exercises}
       </div>
     )
   }
 
   /*Create the Content component */
   const Component = (props) => {
-    return(
+    return (
       <div>
-          <Part part={props.part1} exercises={props.exercises1} />
-          <Part part={props.part2} exercises={props.exercises2} />
-          <Part part={props.part3} exercises={props.exercises3} />        
+        {props.parts.map(part => <Part part={part} />)}
       </div>
     )
   }
-
   
   /*Create the Total component */
   const Total = (props) => {
     /* console.log('Total was used')*/
+    const exercises = props.parts.map(value => value.exercises)
+    let sum = 0
+    exercises.forEach(num =>{
+      sum += num
+    })
     return(
       <div>
         <p>
-          Number of exercises {props.exercises1+props.exercises2+props.exercises3}
+          Number of exercises {sum}
         </p>        
       </div>
     )
@@ -55,10 +65,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Component part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1}  exercises2={exercises2} exercises3={exercises3}/>
+      <Component parts={parts} />
+      <Total parts={parts}/>
     </div>
   )
 }
-
 export default App
