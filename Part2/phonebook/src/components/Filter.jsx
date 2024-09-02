ab3b9216c794bcc64e674c972089937883bd514a
Alesito <alesito@18-242.dynamic.rz.unibw-muenchen.de>
2024-09-02 12:55:09 +0200

    Finished the second part of the exercises

---
+++ Part2/phonebook/src/components/Filter.jsx
@@ -0,0 +1,10 @@
+const Filter = ({filter,handleFilter}) => (
+    <form>
+          <div>
+            filter shown with: <input value = {filter}
+            onChange={handleFilter}/>
+          </div>
+    </form>   
+  )
+
+export default Filter
